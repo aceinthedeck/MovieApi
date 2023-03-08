7519ceb9165e49f882b697baadff0066ecb70dee
Ace <19754841+aceinthedeck@users.noreply.github.com>
2023-03-08 07:57:47 -0500

    added health

---
+++ MovieApi/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.Controllers
+{
+	[Route("health")]
+	public class HealthController : BaseController<HealthController>
+	{
+		public HealthController(ILogger<HealthController> logger) : base(logger)
+		{
+		}
+
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("healthy");
+        }
+    }
+}
+
